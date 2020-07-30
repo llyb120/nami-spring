3f96b2436503abd0b5b525e72e364f956c269cd4
ievi <ievi@live.com>
2020-07-30 21:48:12 +0800

    add test code

---
+++ nami-test/src/main/java/com/github/llyb120/namitest/NamiTestApplication.java
@@ -0,0 +1,44 @@
+package com.github.llyb120.namitest;
+
+import com.github.llyb120.namilite.config.NamiConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.github.llyb120.json.Json.a;
+import static com.github.llyb120.json.Json.o;
+
+@SpringBootApplication
+public class NamiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamiTestApplication.class, args);
+    }
+
+
+    @Bean
+    public NamiConfig namiConfig(){
+        return new NamiConfig(){
+
+            //注册热加载的包
+            @Override
+            public List<String> hotPackages() {
+                return a(
+                    "com.github.llyb120.stock.ctrl"
+                ) ;
+            }
+
+            //0.0.8 新增，现在支持对多个路由和包的映射
+            @Override
+            public Map controlPackages() {
+                return o(
+                    "/api/:c/:a", "com.github.llyb120.stock.ctrl"
+                );
+            }
+        } ;
+    }
+
+}
